6d6881a32e8c88c83f037bf712f164013843b764
Khoa210903 <phamdangkhoa.21092003@gmail.com>
2022-09-05 09:45:55 +0800

    DangKhoa

---
+++ Interchange_Sort.c
@@ -0,0 +1,10 @@
+void InterchangSort(int a[], int N)
+{
+    int i ,j;
+    for(int i=0, i<N, i++)
+        for(int j=i+1, j<=N, j++)
+           if(a[j] < a[i])
+              Hoanvi(a[i], a[j]);
+
+              return 0;
+}
